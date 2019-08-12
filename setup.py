40d1334925ab681cc148d531385790802c9e7ff7
Andreas Hasenkopf <ahasenkopf@suse.de>
2019-08-12 15:54:55 +0200

    Removed support for Python 3.4

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7"
