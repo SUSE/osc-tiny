6f6e68c949f09f813a6bd06be16c0cbd827377bb
Andreas Hasenkopf <ahasenkopf@suse.de>
2022-09-19 14:05:17 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md") as fh:
 
 setup(
     name='osc-tiny',
-    version='0.7.2',
+    version='0.7.3',
     description='Client API for openSUSE BuildService',
     long_description=long_description,
     long_description_content_type="text/markdown",
