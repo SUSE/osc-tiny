d1ad0597c8af5e0cfb1f3966eafbd857156ec522
jonas-brr <jonas.brauer@suse.com>
2025-11-25 10:33:04 +0100

    release 0.11.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md") as fh:
 
 setup(
     name='osc-tiny',
-    version='0.10.8',
+    version='0.11.0',
     description='Client API for openSUSE BuildService',
     long_description=long_description,
     long_description_content_type="text/markdown",
