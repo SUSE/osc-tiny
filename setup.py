93e8de93449236400b362b858965449eee3d147c
Dan Čermák <dcermak@suse.com>
2025-11-28 16:57:38 +0100

    chore: correct description

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.md") as fh:
 setup(
     name='osc-tiny',
     version='0.11.0',
-    description='Client API for openSUSE BuildService',
+    description='Client API for the Open Build Service',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Andreas Hasenkopf',
